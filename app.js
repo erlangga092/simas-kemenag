041d55b67bf4fc18f5776a01c7f3b8d4e29f4952
Erlangga <erlanggaangga092@gmail.com>
2021-09-14 09:14:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,89 @@ const fs = require("fs");
 
 const url = "https://simas.kemenag.go.id/profil/masjid";
 
+const switchProvinsi = (strVal) => {
+  switch (strVal) {
+    case "01":
+      return "ACEH";
+    case "02":
+      return "SUMATERA UTARA";
+    case "03":
+      return "SUMATERA BARAT";
+    case "04":
+      return "RIAU";
+    case "05":
+      return "JAMBI";
+    case "06":
+      return "SUMATERA SELATAN";
+    case "07":
+      return "BENGKULU";
+    case "08":
+      return "LAMPUNG";
+    case "09":
+      return "KEP. BANGKA BELITUNG";
+    case "10":
+      return "KEPULAUAN RIAU";
+    case "11":
+      return "DKI JAKARTA";
+    case "12":
+      return "BANTEN";
+    case "13":
+      return "JAWA BARAT";
+    case "14":
+      return "JAWA TENGAH";
+    case "15":
+      return "D.I YOGYAKARTA";
+    case "16":
+      return "JAWA TIMUR";
+    case "17":
+      return "BALI";
+    case "18":
+      return "NUSA TENGGARA BARAT";
+    case "19":
+      return "NUSA TENGGARA TIMUR";
+    case "20":
+      return "KALIMANTAN BARAT";
+    case "21":
+      return "KALIMANTAN TENGAH";
+    case "22":
+      return "KALIMANTAN SELATAN";
+    case "23":
+      return "KALIMANTAN TIMUR";
+    case "24":
+      return "SULAWESI UTARA";
+    case "25":
+      return "SULAWESI TENGAH";
+    case "26":
+      return "SULAWESI SELATAN";
+    case "27":
+      return "SULAWESI TENGGARA";
+    case "28":
+      return "GORONTALO";
+    case "29":
+      return "SULAWESI BARAT";
+    case "30":
+      return "MALUKU";
+    case "31":
+      return "MALUKU UTARA";
+    case "32":
+      return "PAPUA";
+    case "33":
+      return "PAPUA BARAT";
+    case "34":
+      return "KALIMANTAN UTARA";
+    default:
+      return "-";
+  }
+};
+
+const eksekusiProvinsi = (item) => {
+  if (item.length) {
+    let provinsi = item[1].toString().split(".");
+    let dataProv = switchProvinsi(provinsi[2]);
+    return dataProv;
+  }
+};
+
 const scrapeData = async (arg = 0) => {
   try {
     const { data } = await axios.get(`${url}/${arg}`);
@@ -50,9 +133,10 @@ const olahDataPromise = (data = []) => {
           name: item[0],
           id: item[1],
           address: item[2].toString().replace(/\t/g, "").split("\r\n"),
-          phone: item[3],
+          phone: item[3].toString().replace(/-/g, "").replace(" ", ""),
           email: item[4],
           web: item[5],
+          provinsi: eksekusiProvinsi(item),
         };
       }
     });
@@ -96,4 +180,4 @@ const eksekusiPromise = async (num) => {
   olahDataPromise(resultValue);
 };
 
-eksekusiPromise(200);
+eksekusiPromise(300);
